c57054dfae3831001810984cac033876e845b7d2
bkrug <benjaminkrug@yahoo.com>
2024-02-25 15:39:16 -0500

    Display dragon names from api request

--- dragon-react/src/components/dragon-list/dragon-list.tsx
+++ dragon-react/src/components/dragon-list/dragon-list.tsx
@@ -1,16 +1,32 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './dragon-list.css';
 
+//TODO: Move this to some "model" folder. Add more properties
+class dragon {
+    name: string = "";
+}
+
 function DragonList() {
+    let emptyList: dragon[] = [];
+    let [dragons, setDragons] = useState(emptyList);
+
     useEffect(() => {
         axios.get('http://localhost:5044/dragon/all-dragons?skip=0&take=10')
-            .then(response => console.log(response.data));
+            .then(response => setDragons(response.data));
     }, []);
+
+    let dragonElements = dragons.map(d => (<li>{d.name}</li>));
 
     return (
         <div className='dragon-list'>
             Dragons available for hire!
+            {
+                dragons.length
+            }
+            <ul>
+                { dragonElements }
+            </ul>
         </div>
     );
 };
