7d65856c4e255a7f6c94697c24b99e9b12caa5ad
bkrug <benjaminkrug@yahoo.com>
2024-02-25 23:18:02 -0500

    Use Data Grid Library

--- dragon-react/src/components/dragon-list/dragon-list.tsx
+++ dragon-react/src/components/dragon-list/dragon-list.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
 import axios from 'axios';
 import './dragon-list.css';
 
@@ -20,21 +21,19 @@ function DragonList() {
             .then(response => setDragons(response.data));
     }, []);
 
-    let dragonElements = dragons.map(d => (
-        <div key={d.id} className="dragon-card">
-            <span>{d.name}</span>
-            <span>{d.title}</span>
-            <span>{d.hasFire ? "breathes fire" : "does not breath fire"}</span>
-            <span>{d.hasFlight ? "flies" : "does not fly"}</span>
-        </div>
-    ));
+    const rows: GridRowsProp = dragons;
+      
+      const columns: GridColDef[] = [
+        { field: 'name', headerName: 'Name', width: 150, editable: true },
+        { field: 'title', headerName: 'Title', width: 150, editable: true },
+        { field: 'hasFire', headerName: 'Breathes Fire', width: 150, editable: true },
+        { field: 'hasFlight', headerName: 'Can Fly', width: 150, editable: true },
+      ];
 
     return (
         <div className='dragon-list'>
             Dragons available for hire!
-            <div>
-                { dragonElements }
-            </div>
+            <DataGrid rows={rows} columns={columns} />
         </div>
     );
 };
