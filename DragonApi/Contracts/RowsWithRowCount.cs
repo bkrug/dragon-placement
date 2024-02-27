54c6f46d256c697e25104eaba1fb75fac7ddc968
bkrug <benjaminkrug@yahoo.com>
2024-02-27 09:38:04 -0500

    Move React Logo

--- DragonApi/Contracts/RowsWithRowCount.cs
+++ DragonApi/Contracts/RowsWithRowCount.cs
@@ -3,6 +3,6 @@
     public class RowsWithRowCount<T>
     {
         public int RowCount { get; set; }
-        public IList<T> Items { get; set; }
+        public IList<T> Items { get; set; } = new List<T>();
     }
 }
