f235cb19c0809b948698d9a46c59db218e03726b
bkrug <benjaminkrug@yahoo.com>
2024-02-28 17:01:36 -0500

    Silly code change to show default

--- DragonApi/DragonApi/Controllers/WeatherForecastController.cs
+++ DragonApi/DragonApi/Controllers/WeatherForecastController.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace DragonApi.Controllers
 {
@@ -21,6 +21,16 @@ namespace DragonApi.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            var customer1 = new Customer1();
+            var customer2 = new Customer2();
+            var customer3 = new Customer3();
+            var customers = new ICustomer[] { customer1, customer2, customer3 };
+
+            //DOSN'T COMPILE: var discount1 = customer1.GetLoyaltyDiscount();
+            var discount2 = customer2.GetLoyaltyDiscount();
+            //DOSN'T COMPILE: var discount3 = customer3.GetLoyaltyDiscount();
+            var discounts = customers.Select(c => c.GetLoyaltyDiscount()).ToList();
+
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
@@ -29,5 +39,45 @@ namespace DragonApi.Controllers
             })
             .ToArray();
         }
+    }
+
+    public interface ICustomer
+    {
+        DateTime DateJoined { get; }
+        string Name { get; }
+        // 👇 Provide a body for the new method
+        decimal GetLoyaltyDiscount()
+        {
+            var twoYearsAgo = DateTime.UtcNow.AddYears(-2);
+            if (DateJoined < twoYearsAgo)
+            {
+                // Customers who joined > 2 years ago get a 10% discount
+                return 0.10m;
+            }
+
+            // Otherwise no discount
+            return 0;
+        }
+    }
+
+    public class Customer1 : ICustomer
+    {
+        public DateTime DateJoined { get; set; }
+        public string Name { get; set; }
+    }
+
+
+    public class Customer2 : ICustomer
+    {
+        public DateTime DateJoined { get; set; }
+        public string Name { get; set; }
+        public decimal GetLoyaltyDiscount() => 2;
+    }
+
+    public class Customer3 : ICustomer
+    {
+        public DateTime DateJoined { get; set; }
+        public string Name { get; set; }
+        decimal ICustomer.GetLoyaltyDiscount() => 3;
     }
 }
